7ce07211930a82b28894879a3ce8af6ec1112704
Michael Orlov <orlovm@noexec.org>
2023-02-21 00:00:27 +0200

    Made venv wrapper script more general

--- scripts/venv.sh
+++ scripts/venv.sh
@@ -5,11 +5,16 @@ script_dir=${0%/*}
 venv_dir="${script_dir}"/../venv
 
 if ! [ -e "${venv_dir}" ]; then
-    echo "venv environment not found at ${venv_dir}"
+    echo "venv environment not found at ${venv_dir}" 1>&2
+    exit 1
+fi
+
+if [ $# = 0 ]; then
+    echo "$0 <command> <args>..." 1>&2
     exit 1
 fi
 
 . "${venv_dir}"/bin/activate
 export PYTHONDONTWRITEBYTECODE=1
 
-exec "${venv_dir}"/bin/python "$@"
+exec "$@"
