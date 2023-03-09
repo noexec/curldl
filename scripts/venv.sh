318cb0f21dd596d81dd1136c217f715237416d06
Michael Orlov <orlovm@noexec.org>
2023-03-10 00:00:47 +0200

    Some encoding tweaks

--- scripts/venv.sh
+++ scripts/venv.sh
@@ -22,6 +22,9 @@ export PYTHONPATH="${src_dir}"
 
 export PYTHONDEVMODE=1
 
+export PYTHONIOENCODING=utf-8
+# export PYTHONWARNDEFAULTENCODING=1
+
 # pytest-sugar 0.9.6 (adding it to pytest's filterwarnings is too late to disable the warning)
 export PYTHONWARNINGS="ignore::DeprecationWarning:pytest_sugar,\
     ignore::DeprecationWarning:prompt_toolkit.application.application"
--- tests/functional/test_cli.py
+++ tests/functional/test_cli.py
@@ -13,6 +13,7 @@ import curldl
 def test_run_cli_module(help_switch: str) -> None:
     """Verify that running the module via __main__.py works"""
     package_base_path = pathlib.Path(inspect.getfile(curldl)).parent / os.path.pardir
-    result = subprocess.run(f'python -m {curldl.__package__} {help_switch}', check=True, shell=True, text=True,
-                            capture_output=True, env=dict(os.environ, PYTHONPATH=str(package_base_path)))
+    result = subprocess.run(f'python -m {curldl.__package__} {help_switch}',
+                            check=True, shell=True, text=True, encoding='ascii', capture_output=True,
+                            env=dict(os.environ, PYTHONPATH=str(package_base_path)))
     assert result.stdout.startswith('usage:') and not result.stderr
